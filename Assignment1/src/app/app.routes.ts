8a6132a855435da3811ba45f2baf38413988a08e
PoojanPatel27 <poojanpatel52@gmail.com>
2025-03-04 10:52:08 +0530

    routing done

--- Assignment1/src/app/app.routes.ts
+++ Assignment1/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth-user/login/login.component';
+import { RegisterComponent } from './auth-user/register/register.component';
+import { UserComponent } from './auth-user/user/user.component';
+import { ForgotPasswordComponent } from './auth-user/forgot-password/forgot-password.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path : "login",
+        component : LoginComponent
+    },
+    {
+        path : "register",
+        component : RegisterComponent
+    },
+    {
+        path : "user",
+        component : UserComponent
+    },
+    {
+        path : "forgot-password",
+        component : ForgotPasswordComponent
+    }
+];
